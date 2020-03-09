faff15f5010e7c27da560f08829d9150664e50a0
wanted19901 <wanted19901@gmail.com>
2020-03-09 11:28:25 +0300

    Change price

--- item.rb
+++ item.rb
@@ -10,7 +10,10 @@ attr_reader  :price, :weight
 
 end
 
-item1 = Item.new({:weight =>10, :price =>30})
+item1 = Item.new({:weight =>10, :price =>20})
+item2 = Item.new({:weight =>15, :price =>30})
+item3 = Item.new({:weight =>25, :price =>50})
 puts item1.price
-puts item1.price= 10
+puts item1.weight
+puts item1.price= 30
 puts item1.weight
