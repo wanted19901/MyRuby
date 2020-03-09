2321d3ac487c1f87e3cacab98e0b4bc0cab8deec
wanted19901 <wanted19901@gmail.com>
2020-03-09 11:58:01 +0300

    Price changed

--- item.rb
+++ item.rb
@@ -13,7 +13,5 @@ end
 item1 = Item.new({:weight =>10, :price =>20})
 item2 = Item.new({:weight =>15, :price =>30})
 item3 = Item.new({:weight =>25, :price =>50})
-puts item1.price=10
+puts item1.price=150
 puts item1.weight
-puts item1.price= 30
-puts item2.weight
