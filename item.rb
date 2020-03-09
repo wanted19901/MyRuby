4ef031b7720e4cde1cb093488269cb53e132ca31
wanted19901 <wanted19901@gmail.com>
2020-03-09 11:53:41 +0300

    Puts prise

--- item.rb
+++ item.rb
@@ -13,7 +13,7 @@ end
 item1 = Item.new({:weight =>10, :price =>20})
 item2 = Item.new({:weight =>15, :price =>30})
 item3 = Item.new({:weight =>25, :price =>50})
-puts item1.price
+puts item1.price=10
 puts item1.weight
 puts item1.price= 30
-puts item1.weight
+puts item2.weight
