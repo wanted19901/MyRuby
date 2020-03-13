61c9efb703a08a97c588852efe946ff35511a666
wanted19901 <wanted19901@gmail.com>
2020-03-13 11:00:07 +0300

    Add method Info for class Item

--- init.rb
+++ init.rb
@@ -1,25 +1,10 @@
 require_relative "cart"
 require_relative "item"
 
-cart1 = Cart.new
-cart2 = Cart.new
 
-
-item1 = Item.new({:weight => 100, :price =>200})
+item1 = Item.new({:weight => 100, :price =>200, :name=>"car"})
 item2 = Item.new({:weight => 100, :price =>200})
 item3 = Item.new({:weight => 160, :price =>260})
 
 
-
-#cart1.add_item(Item.new)
-#cart1.add_item(Item.new({:weight =>140}))
-#p cart1.validate
-item1.price= 20
-
-cart2.add_item(item1)
-p item1.weight
-#p cart2.remove_item
-p cart2.items
-cart2.add_item(item3)
-#p cart2.items.shift
-p cart2.items
+item1.info {|attr| puts attr}
--- item.rb
+++ item.rb
@@ -2,8 +2,19 @@ class Item
   def initialize(options={})
     @weight= options[:weight]
     @price = options[:price]
+    @name  = options[:name]
   end
 
-attr_writer :price
-attr_reader  :price, :weight
+ attr_writer :price
+ attr_reader  :price, :weight, :name
+
+ def info
+   yield(price)
+   yield(weight)
+   yield(name)
+ end
+
+
+
+
 end
