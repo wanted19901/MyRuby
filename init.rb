712a224f5d4586afd03fac3358814cbc4eb5f783
wanted19901 <wanted19901@gmail.com>
2020-03-13 11:41:41 +0300

    Testing

--- init.rb
+++ init.rb
@@ -5,12 +5,3 @@ require_relative "item"
 item1 = Item.new({:weight => 100, :price =>200, :name=>"car"})
 item2 = Item.new({:weight => 100, :name =>"ketle"})
 item3 = Item.new({:weight => 160, :price =>260})
-
-
-
-cart1 = Cart.new
-cart1.add_item(item2)
-cart1.add_item(item1)
-p cart1
-cart1.delete_invalid_items
-p cart1
