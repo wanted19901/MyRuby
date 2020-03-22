60df3a6d91b0d1a5f0cd1247238c674f4d5589e7
wanted19901 <wanted19901@gmail.com>
2020-03-22 11:21:39 +0300

    Create module ItemContainer

--- cart.rb
+++ cart.rb
@@ -2,21 +2,10 @@ class Cart
 
   attr_reader :items
 
+  include ItemContainer
+
   def initialize
     @items =[]
-  end
-
-  def add_item(item)
-    @items.push item
-  end
-  def remove_item
-    @items.pop
-  end
-  def validate
-    @items.each {|i| puts "Item has no price" if i.price.nil? }
-  end
-  def delete_invalid_items
-    @items.delete_if {|i| i.price.nil? }
   end
 
 end
--- init.rb
+++ init.rb
@@ -1,14 +1,23 @@
+require_relative "item_container"
 require_relative "cart"
+require_relative "order"
 require_relative "item"
 require_relative "real_item"
 require_relative "virtual_item"
 
 item1 = RealItem.new({:weight => 100, :price =>200, :name=>"car"})
 item2 = VirtualItem.new({:weight => 100, :name =>"ketle"})
-item3 = Item.new({:weight => 160, :price =>260})
+item3 = VirtualItem.new({:weight => 160, :price =>260, :name=>"Dishwasher"})
 
 cart = Cart.new
 cart.add_item(item1)
 cart.add_item(item2)
+cart.remove_item
+puts cart.items.size
 
-puts item1.price 
+order = Order.new
+order.add_item(item1)
+order.add_item(item2)
+order.add_item(item3)
+order.remove_item
+puts order.items.size
